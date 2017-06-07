92dae611a0c42ef52790445f28b71811cff2ce0d
Mike Festger <festger@gmail.com>
2017-06-07 14:01:42 -0500

    index.html App.css App.js index.css
    ADDED:
    
     - Added some style changes
     - Added an image
     - Added skeleton of splash page
    
     TO DO:
    
      - Pretty much everything.

--- src/App.js
+++ src/App.js
@@ -2,24 +2,47 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div style = {titleStyle} id = "2Gather_title">
-            <span style={big2}>2</span>Gather</div>
-      </div>
-    );
-  }
-}
-const titleStyle = {
-    fontFamily: 'Modak',
-    fontSize: 100,
-    color: 'green'
-};
+    render() {
+        return (
+            <div className="App">
+                <div>
+                    <span id = "big2">
+                        2
+                    </span>
+                    <span id = "bigGather">
+                        Gather
+                    </span>
+                    <span id="splashBranchText">
+                        A powerful yet simple event planner.
+                    </span>
+                    <img id="splashBranch1" src="http://clipart.pd4pic.com/images/branch-with-leaves-clipart-1.jpg"></img>
+                </div>
 
-const  big2 = {
-    fontFamily: 'Modak',
-    fontSize: 100,
-    color: 'blue'
-};
+                <div id = "splashGallery">
+                    Pictures will go here, ya?
+                </div>
+
+                <div id = "splashWelcome">
+                    <div className="splashText">
+                        Welcome to 2Gather!
+                    </div>
+                    <div className="splashText">
+                        site description goes here
+                    </div>
+                </div>
+                <div id = "splashFeatures">
+
+                </div>
+
+
+            </div>
+        );
+    }
+}
+
+
+
+
+
+
 export default App;
