67eb1388518c8f8b51213e40f3ed6b5d2cd0cbce
Mike Festger <festger@gmail.com>
2017-06-07 21:38:25 -0500

    index.html App.css App.js
    ADDED:
    
     - Added some style changes
     - Experimenting with font awesome + react.js
    
     TO DO:
    
      - Pretty much everything.

--- src/App.js
+++ src/App.js
@@ -5,39 +5,48 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+
+
                 <div>
-                    <span id = "big2">
+                    <span id="big2">
                         2
                     </span>
-                    <span id = "bigGather">
+                    <span id="bigGather">
                         Gather
                     </span>
+                    <a href={this.props.url}>{this.props.text}</a>
                     <span id="splashBranchText">
                         A powerful yet simple event planner.
                     </span>
-                    <img id="splashBranch1" src="http://clipart.pd4pic.com/images/branch-with-leaves-clipart-1.jpg"></img>
+                    <img id="splashBranch1"
+                         src="http://clipart.pd4pic.com/images/branch-with-leaves-clipart-1.jpg"></img>
                 </div>
 
-                <div id = "splashGallery">
-                    Pictures will go here, ya?
-                </div>
+                {/*<div id="splashGallery">*/}
+                    {/*Pictures will go here, ya?*/}
+                {/*</div>*/}
 
-                <div id = "splashWelcome">
+                <div id="splashWelcome">
                     <div className="splashText">
                         Welcome to 2Gather!
                     </div>
                     <div className="splashText">
-                        site description goes here
+                        Plan events, coordinate details, and centralize your event planning needs in a convenient and easy to use tool.
                     </div>
                 </div>
-                <div id = "splashFeatures">
-
+                <div id="splashFeatures">
+                    <i classname="fa fa-leaf" aria-hidden="true"> </i>
+                    Plan an event
                 </div>
 
 
             </div>
-        );
+
+
+    );
+
     }
+
 }
 
 
